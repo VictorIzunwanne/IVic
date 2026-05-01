a933c2274de5da2e6370fa1acbb34b8e7ea3866b
Izunwanne Victor <victorizunwanneweb@gmail.com>
2026-05-01 10:08:11 +0100

    First, I got the elements to change positioning while remaining in the same place then I finally got it to move to the top left of the screen and expand as needed.

--- projects/disneyCharacters/script.js
+++ projects/disneyCharacters/script.js
@@ -31,12 +31,12 @@ let charDs = document.querySelectorAll(".char-d");
 // Showing the "read more" button when the card is hovered
 charDs.forEach((charD) => {
   let readMore = charD.querySelector(".more");
-  charD.addEventListener("mouseover", () => {
+  charD.addEventListener("mouseenter", () => {
     readMore.style.blockSize = "1.5rem";
   });
 
   // hiding the "read more" button when the card is not hovered
-  charD.addEventListener("mouseout", () => {
+  charD.addEventListener("mouseleave", () => {
     readMore.style.blockSize = "0";
   });
 });
@@ -49,20 +49,23 @@ let main = document.querySelector("main");
 let charImgs = document.querySelectorAll(".char-img");
 
 // Selecting the individual cards
-charImgs.forEach((cha) => {
-  let mainScr = main.scrollLeft;
-  let chaRect = cha.getBoundingClientRect().x;
+window.addEventListener("load", () => {
+  charImgs.forEach((cha) => {
+    let mainScr = main.scrollLeft;
+    let chaRect = cha.getBoundingClientRect().x;
 
-  // Increasing the image size when the cards are in view
-  if (chaRect > 100 && chaRect < 1200) {
-    cha.style.transform = "scale(1)";
-    cha.style.marginBottom = "0";
+    // Increasing the image size when the cards are in view
 
-    return;
-  }
-  // Reducing the image size when the cards are not in view
-  cha.style.transform = "scale(0.8)";
-  cha.style.marginBottom = "-2rem";
+    if (chaRect > 100 && chaRect < 1200) {
+      cha.style.transform = "scale(1)";
+      cha.style.marginBottom = "0";
+
+      return;
+    }
+    // Reducing the image size when the cards are not in view
+    cha.style.transform = "scale(0.8)";
+    cha.style.marginBottom = "-2rem";
+  });
 });
 
 // On Document Scroll X
@@ -81,5 +84,42 @@ main.addEventListener("scroll", () => {
     // Reducing the image size when it is not in view
     cha.style.transform = "scale(0.8)";
     cha.style.marginBottom = "-2rem";
+  });
+});
+
+// SHOWING CHARACTER DETAILS
+
+// Declaring the "read more buttons"
+let mores = document.querySelectorAll(".more");
+
+mores.forEach((i) => {
+  i.addEventListener("click", () => {
+    let header = document.querySelector("header");
+    let footer = document.querySelector("footer");
+
+    let parent = i.parentElement.parentElement;
+
+    header.style.blockSize = 0;
+    footer.style.blockSize = 0;
+
+    let parentPos = parent.getBoundingClientRect();
+
+    i.style.display = "none";
+    parent.style.position = "fixed";
+    parent.style.left = parentPos.left + "px";
+    parent.style.top = parentPos.top + "px";
+    parent.style.transition = "all 0.5s";
+
+    setTimeout(() => {
+      parent.style.left = "10rem";
+      parent.style.top = "10rem";
+    }, 500);
+
+    setTimeout(() => {
+      parent.style.transition = "all 1s";
+      parent.style.borderRadius = "10rem";
+      parent.style.blockSize = "100rem";
+      parent.style.inlineSize = "100rem";
+    }, 1000);
   });
 });
